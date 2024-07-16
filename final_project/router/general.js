adbce9bb396d3cbdb05c5affae50e59a2fcefaf4
cots-mb <otsubo@meta-blank.com>
2024-07-16 10:09:41 +0100

    3-6 Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,20 +28,52 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+   let result =[];
+
+  for(let i=1;i<11;i++){
+        if(books[i].author===author){ 
+           result.push(books[i]);
+        } 
+    }
+    if(result.length>0){
+        return res.status(200).send(JSON.stringify(result));
+    }else{
+        return res. status (300).json({message: "No-book for this author found" }) ;
+    }
+ // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+   let result =[];
+
+  for(let i=1;i<11;i++){
+        if(books[i].title===title){ 
+           result.push(books[i]);
+        } 
+    }
+    if(result.length>0){
+        return res.status(200).send(JSON.stringify(result));
+    }else{
+        return res. status (300).json({message: "No-book for this title found" }) ;
+    }
+ // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let lkeys = Object.keys(books[isbn].reviews);
+  if(lkeys.length === 0){
+    return res.status(300).json({message: "No reviews available"});
+}else{
+    return res.status(200).json(books[isbn].reviews);
+    }
+ // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
